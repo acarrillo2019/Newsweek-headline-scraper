16abd8c26a6597eaf4c2baec11e41684764a0be6
acarrillo2019 <alric21@aol.com>
2020-04-11 03:45:02 -0700

    server.js

--- server.js
+++ server.js
@@ -17,27 +17,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-
-mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
-// Setup access to static assets
-app.use(express.static("public"));
-
-// Setup Handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Configure routes
 require("./routes/routes.js")(app, axios, cheerio, db, mongoose);
 
-// Start the server
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
