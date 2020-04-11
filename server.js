6370eb55b35f5dc71cee4a9d327f2882b35583a6
acarrillo2019 <alric21@aol.com>
2020-04-11 04:24:40 -0700

    Adjusted server js

--- server.js
+++ server.js
@@ -17,9 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise =global.Promise;
 
-mongoose.connect(MONGODB_URI);
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:password1@ds153667.mlab.com:53667/heroku_615lm3bs";
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true
+});
 
 require("./routes/routes.js")(app, axios, cheerio, db, mongoose);
 
